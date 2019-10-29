541f0f000c717c9bb7ded10fa4a4aebc73609555
sbi77893 <56716502+sbi77893@users.noreply.github.com>
2019-10-29 13:45:06 +0500

    Update _DAL.cs

--- DAL/_DAL.cs
+++ DAL/_DAL.cs
@@ -1,12 +1,9 @@
-﻿using RfReader_demo.BAL;
+using RfReader_demo.BAL;
 using Sentry;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Data;
-using System.Data.OracleClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using static RfReader_demo.BAL._BAL;
 
 namespace RfReader_demo.DAL
@@ -15,23 +12,32 @@ namespace RfReader_demo.DAL
     {
         public virtual string InsertDataToTable(TableData tblData)
         {
-            using (SentrySdk.Init(MainWindow.conn))
+            string Message = string.Empty;
+            try
             {
-                try
+                string conn = ConfigurationManager.ConnectionStrings["DSN"].ConnectionString;
+                if (!string.IsNullOrEmpty(conn))
                 {
-                    var dt = tblData.CheckTime.ToString("dd/MM/yyyy HH:mm:ss");
-                    string query = "Insert Into " + tblData.TableName + " (RFID, ScanTime) Values ('" + tblData.RFID + "', To_Date('" + dt + "', 'DD/MM/YYYY HH24:MI:SS'))";
-                    //OracleHelper.ExecuteNonQuery(_BAL.StrConnection, CommandType.Text, query).ToString();            
-                    OracleHelper.ExecuteNonQuery(MainWindow.ConnectionString, CommandType.Text, query).ToString();
-                    return "scan";
-
+                    SentrySdk.Init(conn);
                 }
-                catch (Exception ex)
+                var dt = tblData.CheckTime.ToString("dd/MM/yyyy HH:mm:ss");
+                string query = "Insert Into " + tblData.TableName + " (RFID, ScanTime) Values ('" + tblData.RFID + "', To_Date('" + dt + "', 'DD/MM/YYYY HH24:MI:SS'))";        
+                int ID =  OracleHelper.ExecuteNonQuery(MainWindow.ConnectionString, CommandType.Text, query);
+                if (ID == 1)
                 {
-                    SentrySdk.CaptureException(ex);
-                    return ex.Message;
+                    Message = "Data Inserted Successfully";
+                }
+                else
+                {
+                    Message = "Cannot Insert data in table";
                 }
             }
+            catch (Exception ex)
+            {
+                Message = ex.Message;
+                SentrySdk.CaptureException(ex);
+            }
+            return Message;
         }
 
     }
